84df9fc10305b2201c2ad29f5f61fc8b7b42be71
codercetc <105865533+codercetc@users.noreply.github.com>
2023-07-15 14:47:51 +0800

    第一次提交

---
+++ src/com/coder/Test.java
@@ -0,0 +1,12 @@
+package com.coder;
+
+/**
+ * @author teachershi
+ * @project com.coder
+ * @date 2023/7/15
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
