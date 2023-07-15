96147b091d832ec79c2c5da7bbf414334edd82da
codercetc <105865533+codercetc@users.noreply.github.com>
2023-07-15 16:38:41 +0800

    Update Test.java
    web modify

--- src/com/coder/Test.java
+++ src/com/coder/Test.java
@@ -13,5 +13,6 @@ public class Test {
         System.out.println("hello fix branch");
         System.out.println("hello dev branch");
         System.out.println("hello github");
+        System.out.println("hello github web");
     }
 }
